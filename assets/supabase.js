8a6cf0a50065d51c0a284f20738fc134072a80b4
akashmakku2018-a11y <akashmakku2018@gmail.com>
2026-08-17 10:12:00 +0300

    Create supabase.js

---
+++ assets/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = 'https://jzsmmqkrkrdryjqvvabc.supabase.co';
+
+const SUPABASE_ANON_KEY =
+  'sb_publishable_Fm7fEYOaW3IRXPAZ2uwzbA_DDJ1ivdH';
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
